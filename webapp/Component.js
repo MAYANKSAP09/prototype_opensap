0ebcd3f202c2f6d02c59699dcd7e7a58f19cdabf
Mayank Thakur <er.mayanksap@yahoo.in>
2019-05-14 14:36:33 +0000

    Initial First Commit Test

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("mayank.salesorderfrombopf.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("mayank.salesorderfrombopf.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
